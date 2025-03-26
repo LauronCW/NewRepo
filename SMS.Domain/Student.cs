cc90e886ec8ebeb73373424612c0de93fea9909c
LauronCW <c.lauron.560394@umindano.edu.ph>
2025-03-26 14:25:43 +0800

    Revert "added SMS files"
    This reverts commit 0a625b2e2ca584f4b5e20087d64341602b772a4a.

--- SMS.Domain/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SMS.Domain
-{
-    public class Student
-    {
-        public int StudentId { get; set; }
-        public string StudentName { get; set; }
-        public string? Email { get; set; }
-    }
-}
