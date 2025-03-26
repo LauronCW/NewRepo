2f9b809244d1ee9712361081d8de9720a0c8bdaf
LauronCW <c.lauron.560394@umindano.edu.ph>
2025-03-26 15:13:17 +0800

    march 26 files

--- SMS.App/Form1.Designer.cs
+++ SMS.App/Form1.Designer.cs
@@ -35,6 +35,7 @@
             textBoxName = new TextBox();
             label1 = new Label();
             buttonShow = new Button();
+            labelAddedInformation = new Label();
             SuspendLayout();
             // 
             // textBoxId
@@ -95,11 +96,21 @@
             buttonShow.UseVisualStyleBackColor = true;
             buttonShow.Click += buttonShow_Click;
             // 
+            // labelAddedInformation
+            // 
+            labelAddedInformation.AutoSize = true;
+            labelAddedInformation.Location = new Point(207, 216);
+            labelAddedInformation.Name = "labelAddedInformation";
+            labelAddedInformation.Size = new Size(108, 15);
+            labelAddedInformation.TabIndex = 9;
+            labelAddedInformation.Text = "Added Information";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(600, 272);
+            Controls.Add(labelAddedInformation);
             Controls.Add(buttonShow);
             Controls.Add(label1);
             Controls.Add(textBoxName);
@@ -123,5 +134,6 @@
         private TextBox textBoxName;
         private Label label1;
         private Button buttonShow;
+        private Label labelAddedInformation;
     }
 }
